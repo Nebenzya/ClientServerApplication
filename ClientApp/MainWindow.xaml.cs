84906e4429c186955fe585c009f991af45842b1d
aleksei6191@gmail.com <aleksei6191@gmail.com>
2022-04-12 18:04:13 +0300

    add Client App  prototype

---
+++ ClientApp/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace ClientApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Exit_Menu(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
